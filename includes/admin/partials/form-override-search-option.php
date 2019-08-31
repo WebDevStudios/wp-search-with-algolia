6c3f8a8f3fa3d71397adc4795dccd1aeb1949abb
Richard Aber <richard.aber@webdevstudios.com>
2019-08-31 15:31:56 -0500

    #3 Make Search Page radio button descriptions translatable

--- includes/admin/partials/form-override-search-option.php
+++ includes/admin/partials/form-override-search-option.php
@@ -1,42 +1,59 @@
-
 <div class="input-radio">
 	<label>
-		<input type="radio" value="native" name="algolia_override_native_search" <?php if ( 'native' === $value ) :
-?>checked<?php endif; ?>>
+		<input type="radio" value="native"
+			name="algolia_override_native_search" <?php checked( $value, 'native' ); ?>>
 		<?php esc_html_e( 'Do not use Algolia', 'wp-search-with-algolia' ); ?>
 	</label>
 	<div class="radio-info">
-		Do not use Algolia for searching at all.
-		This is only a valid option if you wish to search on your content from another website.
+		<?php
+		echo wp_kses(
+			__(
+				'Do not use Algolia for searching at all.<br/>This is only a valid option if you wish to search on your content from another website.',
+				'wp-search-with-algolia'
+			),
+			[
+				'br' => [],
+			]
+		);
+		?>
 	</div>
 
 	<label>
-		<input type="radio" value="backend" name="algolia_override_native_search" 
-		<?php
-		if ( 'backend' === $value ) :
-?>
-checked<?php endif; ?>>
+		<input type="radio" value="backend"
+			name="algolia_override_native_search" <?php checked( $value, 'backend' ); ?>>
 		<?php esc_html_e( 'Use Algolia in the backend', 'wp-search-with-algolia' ); ?>
 	</label>
 	<div class="radio-info">
-		With this option WordPress search will be powered by Algolia behind the scenes.
-		This will allow your search results to be typo tolerant.
-		<b>This option does not support filtering and displaying instant search results but has the advantage to play nicely with any theme.</b>
+		<?php
+		echo wp_kses(
+			__(
+				'With this option WordPress search will be powered by Algolia behind the scenes.<br/>This will allow your search results to be typo tolerant.<br/><b>This option does not support filtering and displaying instant search results but has the advantage to play nicely with any theme.</b>',
+				'wp-search-with-algolia'
+			),
+			[
+				'br' => [],
+				'b'  => [],
+			]
+		);
+		?>
 	</div>
 
 	<label>
-		<input type="radio" value="instantsearch" name="algolia_override_native_search" 
-		<?php
-		if ( 'instantsearch' === $value ) :
-?>
-checked<?php endif; ?>>
+		<input type="radio" value="instantsearch"
+			name="algolia_override_native_search" <?php checked( $value, 'instantsearch' ); ?>>
 		<?php esc_html_e( 'Use Algolia with Instantsearch.js', 'wp-search-with-algolia' ); ?>
 	</label>
 	<div class="radio-info">
-		This will replace the search page with an instant search experience powered by Algolia.
-		By default you will be able to filter by post type, categories, tags and authors.
-
-		Please note that the plugin is shipped with some sensible default styling rules but it could require some
-		CSS adjustments to provide an optimal search experience.
+		<?php
+		echo wp_kses(
+			__(
+				'This will replace the search page with an instant search experience powered by Algolia.<br/>By default you will be able to filter by post type, categories, tags and authors.<br/>Please note that the plugin is shipped with some sensible default styling rules<br/>but it could require some CSS adjustments to provide an optimal search experience.',
+				'wp-search-with-algolia'
+			),
+			[
+				'br' => [],
+			]
+		);
+		?>
 	</div>
 </div>
