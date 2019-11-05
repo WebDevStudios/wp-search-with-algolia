126b7c3999f7ae5f73a1419bec36f61302cfd86a
Greg Rickaby <greg@gregrickaby.com>
2019-11-05 14:45:20 -0600

    move options to /admin

---
+++ src/admin/class-options.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WebDevStudios\WPSWA\Admin;
+
+class Options {
+
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'register_menu_page' ) );
+	}
+
+	public function register_menu_page() {
+		add_menu_page( 'Custom Menu Page Title', 'Custom Menu Page', 'manage_options', 'custom.php', '', 'dashicons-welcome-widgets-menus', 90 );
+	}
+
+	public static function run() {
+		$init = new Options();
+	}
+}
