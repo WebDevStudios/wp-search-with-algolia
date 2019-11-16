f812e89da00224c7d98aac98a5dcf65069f3d446
Richard Aber <richard.aber@webdevstudios.com>
2019-11-16 00:35:52 -0600

    #20 Remove WPSearchWithAlgolia class from plugin bootstrap
    Add a message to admin if autoloader is not readable and self-deactivate.
    Use PluginFactory to instantiate Plugin.
    Reaffirm the PHP 7.2 and WP 5.0 requirements.

--- wp-search-with-algolia.php
+++ wp-search-with-algolia.php
@@ -1,154 +1,95 @@
 <?php
 /**
+ * WP Search with Algolia plugin bootstrap file.
+ *
+ * @since   1.0.0
+ * @package WebDevStudios\WPSWA
+ *
  * @wordpress-plugin
  * Plugin Name:       WP Search with Algolia
  * Plugin URI:        https://github.com/WebDevStudios/wp-search-with-algolia
  * Description:       Integrate the powerful Algolia search service with WordPress.
  * Version:           2.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      7.2
  * Author:            WebDevStudios
  * Author URI:        https://webdevstudios.com
  * License:           GNU General Public License v3.0 / MIT License
  * Text Domain:       wp-search-with-algolia
- * Domain Path:       /languages/
+ * Domain Path:       /languages
  */
 
 namespace WebDevStudios\WPSWA;
 
+use WebDevStudios\WPSWA\{
+	PluginFactory,
+	Plugin,
+	CLI\Algolia
+};
+
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if ( ! \defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// Plugin version.
+\define( 'WPSWA_PLUGIN_VERSION', '2.0.0' );
+
+// Plugin bootstrap file.
+\define( 'WPSWA_PLUGIN_FILE', __FILE__ );
+
+// Plugin directory path.
+\define( 'WPSWA_PLUGIN_DIR', \plugin_dir_path( WPSWA_PLUGIN_FILE ) );
+
+// Plugin directory URL.
+\define( 'WPSWA_PLUGIN_URL', \plugins_url( '', WPSWA_PLUGIN_FILE ) );
+
 /**
- * The core plugin class.
+ * The full path to the PSR4 autoloader.
  *
- * @since      2.0.0
+ * @since 2.0.0
+ *
+ * @var string $wpswa_autoloader
  */
-final class WPSearchWithAlgolia {
+$wpswa_autoloader = __DIR__ . '/vendor/autoload.php';
 
-	/**
-	 * Stores the instance of the WPSearchWithAlgolia class.
-	 *
-	 * @var      WPSearchWithAlgolia The one true WPSearchWithAlgolia
-	 * @since    2.0.0
-	 * @access   private
-	 */
-	private static $instance;
+/*
+ * If the autloader is not readable,
+ * setup an admin notice, deactivate, and return early,
+ * so the site will continue to function.
+ */
+if ( ! \is_readable( $wpswa_autoloader ) ) {
 
-	/**
-	 * Create an object, only if the class has no instance.
-	 *
-	 * @since    2.0.0
-	 */
-	public static function instance() {
-
-		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof WPSearchWithAlgolia ) ) {
-			self::$instance = new WPSearchWithAlgolia();
-			self::$instance->setup_constants();
-			self::$instance->includes();
-			self::$instance->admin_hooks();
-			self::$instance->public_hooks();
+	\add_action(
+		'admin_notices',
+		function () {
+			$notice = \__(
+				'WP Search with Algolia cannot be activated, the autoloader is not readable.',
+				'wp-search-with-algolia'
+			);
+			echo '<div class="notice notice-error"><p>' . \esc_html( $notice ) . '</p></div>';
 		}
+	);
 
-		return self::$instance;
-	}
+	\add_action(
+		'admin_init',
+		function () {
+			\deactivate_plugins(
+				\plugin_basename( WPSWA_PLUGIN_FILE )
+			);
+		}
+	);
 
-	/**
-	 * Define and set up constants.
-	 *
-	 * @since    2.0.0
-	 * @access   private
-	 */
-	private function setup_constants() {
-
-		// Plugin version.
-		define( 'WPSWA_PLUGIN_VERSION', '2.0.0' );
-
-		// Plugin folder path.
-		define( 'WPSWA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-
-		// Plugin folder URL.
-		define( 'WPSWA_PLUGIN_URL', plugins_url( '', __FILE__ ) );
-
-		// Plugin root file.
-		define( 'WPSWA_PLUGIN_FILE', __FILE__ );
-
-		// Disable DEBUG mode.
-		define( 'WPSWA_DEBUG', false );
-	}
-
-	/**
-	 * Use Composer to autoload our dependencies.
-	 *
-	 * @since    2.0.0
-	 * @access   private
-	 */
-	private function includes() {
-		require_once __DIR__ . '/vendor/autoload.php';
-	}
-
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    2.0.0
-	 */
-	public function admin_hooks() {
-		add_action( 'admin_menu', array( $this, 'options_page' ) );
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    2.0.0
-	 */
-	public function public_hooks() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
-		add_action( 'plugins_loaded', array( $this, 'textdomain' ) );
-	}
-
-	/**
-	 * Register scripts so that they can be used in other plugins outside
-	 * of the context of the core features.
-	 *
-	 * @since    2.0.0
-	 */
-	public function scripts() {
-		wp_register_script( 'algolia-search', WPSWA_PLUGIN_URL . '/dist/algolia.bundle.js', array(), '2.0.0', true );
-		wp_enqueue_script( 'algolia-search' );
-	}
-
-	/**
-	 * Register and load translation file(s).
-	 *
-	 * @since    2.0.0
-	 */
-	public function textdomain() {
-		load_plugin_textdomain( 'wp-search-with-algolia', false, WPSWA_PLUGIN_URL . '/src/languages' );
-	}
-
-	/**
-	 * Register options page.
-	 *
-	 * @since    2.0.0
-	 */
-	public function options_page() {
-		add_options_page( esc_html__( 'Algolia Settings', 'wp-search-with-algolia' ), esc_html__( 'Algolia Settings', 'wp-search-with-algolia' ), 'manage_options', 'wpswa', 'wpswa_option_page' );
-	}
+	return;
 }
 
-/**
- * Initialize our plugin.
- *
- * @since    2.0.0
- */
-function init() {
-	return WPSearchWithAlgolia::instance();
-}
-init();
+// Require the autoloader.
+require $wpswa_autoloader;
 
-// Maybe register WP-CLI commands.
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	require_once 'cli/class-wp-cli.php';
+// Run the plugin.
+( PluginFactory::create() )->register();
+
+// Add WP-CLI commands.
+if ( \defined( 'WP_CLI' ) && WP_CLI ) {
+	\WP_CLI::add_command( 'algolia', new Algolia() );
 }
