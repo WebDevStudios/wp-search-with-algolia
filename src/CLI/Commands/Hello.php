0bc5761994a64203cf5fc5a916d6e968e7af95e6
Richard Aber <richard.aber@webdevstudios.com>
2019-11-22 22:44:50 -0600

    #20 Hello command should probably do some check engines work

--- src/CLI/Commands/Hello.php
+++ src/CLI/Commands/Hello.php
@@ -10,6 +10,9 @@
 
 namespace WebDevStudios\WPSWA\CLI\Commands;
 
+use \WP_CLI;
+use \WP_CLI_Command;
+
 /**
  * Verify that the Algolia CLI commands have loaded.
  *
@@ -21,7 +24,7 @@ namespace WebDevStudios\WPSWA\CLI\Commands;
  *
  * @since 2.0.0
  */
-class Hello extends \WP_CLI_Command {
+class Hello extends WP_CLI_Command {
 
 	/**
 	 * Verify that the Algolia CLI commands have loaded.
@@ -30,6 +33,23 @@ class Hello extends \WP_CLI_Command {
 	 * @since   2.0.0
 	 */
 	public function hello() {
-		\WP_CLI::success( 'Algolia CLI Command is correctly loaded 🎉' );
+
+		$has_errors = false;
+		$app_id     = \get_option( 'algolia_application_id', '' );
+		$api_key    = \get_option( 'algolia_api_key', '' );
+
+		if ( empty( $app_id ) ) {
+			$has_errors = true;
+			WP_CLI::error( 'Missing App ID.' );
+		}
+
+		if ( empty( $api_key ) ) {
+			$has_errors = true;
+			WP_CLI::error( 'Missing API key.' );
+		}
+
+		if ( ! $has_errors ) {
+			WP_CLI::success( 'Algolia CLI Command is correctly loaded 🎉' );
+		}
 	}
 }
