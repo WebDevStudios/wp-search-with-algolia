a7b1d2a4d008d4df80056cd807045c87cb138b58
Richard Aber <richard.aber@webdevstudios.com>
2019-11-29 20:19:14 -0600

    #20 Remove the options page from the main plugin class

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,7 +11,8 @@ namespace WebDevStudios\WPSWA;
 use WebDevStudios\WPSWA\{
 	Structure\Plugin\Plugin as OopsPlugin,
 	Services\LoadTextDomain,
-	Services\EnqueueScripts
+	Services\EnqueueScripts,
+	Services\Admin\Options
 };
 
 /**
@@ -26,14 +27,14 @@ final class Plugin extends OopsPlugin {
 	/**
 	 * Array of services.
 	 *
-	 * @todo Really would prefer a container.
+	 * @since 2.0.0
 	 *
-	 * @since  2.0.0
 	 * @var mixed
 	 */
 	protected $services = [
 		LoadTextDomain::class,
 		EnqueueScripts::class,
+		Options::class,
 	];
 
 	/**
@@ -43,32 +44,5 @@ final class Plugin extends OopsPlugin {
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 */
 	public function __construct() {
-		$this->admin_hooks(); // @todo This doesn't belong here. #25 -- figure out what we're doing with options page.
-	}
-
-	/**
-	 * Register all of the hooks related to the admin area functionality of the plugin.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function admin_hooks(): void {
-		\add_action( 'admin_menu', [ $this, 'options_page' ] );
-	}
-
-	/**
-	 * Register options page.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function options_page(): void {
-		\add_options_page(
-			\esc_html__( 'Algolia Settings', 'wp-search-with-algolia' ),
-			\esc_html__( 'Algolia Settings', 'wp-search-with-algolia' ),
-			'manage_options',
-			'wpswa',
-			'wpswa_option_page'
-		);
 	}
 }
