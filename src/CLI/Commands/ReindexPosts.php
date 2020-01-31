f80bf2034b39e9721fd35ef6a3ddb5b34e87ed64
Richard Aber <richard.aber@webdevstudios.com>
2020-01-31 10:23:30 -0600

    #20 Inject SearchClient into ReindexPosts command

--- src/CLI/Commands/ReindexPosts.php
+++ src/CLI/Commands/ReindexPosts.php
@@ -14,7 +14,8 @@ use \WP_Query;
 use \WP_CLI;
 use \WP_CLI_Command;
 use \InvalidArgumentException;
-use WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\SearchClient;
+use \WebDevStudios\WPSWA\CLI\AlgoliaCLI;
+use \WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\SearchClient;
 
 /**
  * Reindex posts.
@@ -23,14 +24,33 @@ use WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\SearchClient;
  *
  *     # Reindex posts.
  *     $ wp algolia reindex_posts
- *     Success: Algolia CLI Command is correctly loaded
+ *     Success: Posts reindexed
  *
  * @since 2.0.0
  */
-class ReindexPosts extends WP_CLI_Command {
+class ReindexPosts extends AlgoliaCLI {
+
+	/**
+	 * The Aglolia SearchClient.
+	 *
+	 * @Inject
+	 * @var SearchClient
+	 */
+	protected $search_client;
+
+	/**
+	 * ReindexPosts constructor.
+	 *
+	 * @param SearchClient $search_client
+	 */
+	public function __construct( SearchClient $search_client ) {
+		$this->search_client = $search_client;
+	}
 
 	/**
 	 * Reindex posts method.
+	 *
+	 * @todo This needs _a lot_ of work before it is usable.
 	 *
 	 * @author  WebDevStudios <contact@webdevstudios.com>
 	 * @since   2.0.0
@@ -42,22 +62,11 @@ class ReindexPosts extends WP_CLI_Command {
 	 */
 	public function reindex_posts( $args, $assoc_args ): void {
 
-		$app_id  = \get_option( 'algolia_application_id', '' );
-		$api_key = \get_option( 'algolia_api_key', '' );
-
-		if ( empty( $app_id ) || empty( $api_key ) ) {
-			WP_CLI::error( 'Missing App ID or API key' );
-
-			return;
-		}
-
-		$this->algolia_search_client = SearchClient::create( $app_id, $api_key );
-
 		global $table_prefix; // @todo -- Why is this here? What are we using this for?
 
 		$type       = isset( $assoc_args['type'] ) ? $assoc_args['type'] : 'post';
 		$index_name = $table_prefix . $type;
-		$index      = $this->algolia_search_client->initIndex(
+		$index      = $this->search_client->initIndex(
 			\apply_filters( 'algolia_index_name', $index_name, $type ) // phpcs:ignore
 		);
 
