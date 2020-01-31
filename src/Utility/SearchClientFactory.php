140d3dfd76562faf1807dc006817bbed5ceb6109
Richard Aber <richard.aber@webdevstudios.com>
2020-01-31 10:06:02 -0600

    #20 Add a SearchClientFactory

---
+++ src/Utility/SearchClientFactory.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * SearchClientFactory class file.
+ *
+ * @since   2.0.0
+ * @package WebDevStudios\WPSWA\Utility
+ */
+
+namespace WebDevStudios\WPSWA\Utility;
+
+use WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\Algolia;
+use WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\Http\HttpClientInterface;
+use \WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\SearchClient;
+use \WDS_WPSWA_Vendor\Algolia\AlgoliaSearch\Config\SearchConfig;
+
+/**
+ * Class SearchClientFactory
+ *
+ * Responsible for creating a shared instance of the Algolia SearchClient object.
+ *
+ * @since 2.0.0
+ */
+final class SearchClientFactory {
+
+	/**
+	 * Create and return an instance of the SearchClient.
+	 *
+	 * @author WebDevStudios <contact@webdevstudios.com>
+	 * @since  2.0.0
+	 *
+	 * @param SearchConfig        $search_config The Algolia Search Config instance.
+	 * @param HttpClientInterface $http_client   An HttpClientInterface object.
+	 *
+	 * @return SearchClient The shared SearchClient instance.
+	 */
+	public static function create( SearchConfig $search_config, HttpClientInterface $http_client ): SearchClient {
+
+		/**
+		 * The static instance to share, else null.
+		 *
+		 * @var null|SearchClient $search_client
+		 */
+		static $search_client = null;
+
+		if ( null !== $search_client ) {
+			return $search_client;
+		}
+
+		/**
+		 * > The PHP client allows you to override what http layer
+		 * > will be used by the SearchClient (and the other clients).
+		 * > You need to set it in the Algolia class before instantiating your client.
+		 *
+		 * @link   https://www.algolia.com/doc/api-client/advanced/pass-options-to-the-http-client/php/?language=php
+		 */
+		Algolia::setHttpClient( $http_client );
+
+		$search_client = SearchClient::createWithConfig( $search_config );
+
+		return $search_client;
+	}
+}
