d04e115fd61192a8e629e9d91174591266ac230c
Richard Aber <richard.aber@webdevstudios.com>
2020-01-31 23:14:18 -0600

    #20 Remove all the option fields from Options

--- src/Services/Admin/Options.php
+++ src/Services/Admin/Options.php
@@ -8,6 +8,7 @@
 
 namespace WebDevStudios\WPSWA\Services\Admin;
 
+use \WebDevStudios\WPSWA\Services\Admin\Settings\AlgoliaApplicationId;
 use \WebDevStudios\WPSWA\Structure\Service;
 use \WebDevStudios\WPSWA\Utility\AlgoliaSettings;
 
@@ -84,11 +85,22 @@ class Options extends Service {
 	 */
 	public function register_hooks(): void {
 		\add_action( 'admin_menu', [ $this, 'admin_menu' ] );
-		\add_action( 'admin_init', [ $this, 'admin_init' ] );
+		\add_action( 'admin_init', [ $this, 'add_settings_section' ] );
+		\add_action( 'admin_notices', array( $this, 'settings_errors' ) );
 		\add_filter(
 			'plugin_action_links_' . \plugin_basename( WPSWA_PLUGIN_FILE ),
 			[ $this, 'add_plugin_settings_action_link' ]
 		);
+	}
+
+	/**
+	 * Display settings errors.
+	 *
+	 * @since  2.0.0
+	 * @author WebDevStudios <contact@webdevstudios.com>
+	 */
+	public function settings_errors(): void {
+		\settings_errors( $this->option_group );
 	}
 
 	/**
@@ -101,11 +113,21 @@ class Options extends Service {
 	 *
 	 * @return array
 	 */
-	public function add_plugin_settings_action_link( array $actions ) {
+	public function add_plugin_settings_action_link( array $actions ): array {
+
+		$settings_url = \add_query_arg(
+			[
+				'page' => $this->slug,
+			],
+			\admin_url( 'admin.php' )
+		);
+
 		return \array_merge(
 			$actions,
 			[
-				'<a href="' . \esc_url( \admin_url( 'admin.php?page=' . $this->slug ) ) . '">' . \esc_html__( 'Settings', 'wp-search-with-algolia' ) . '</a>',
+				'<a href="' . \esc_url( $settings_url ) . '">'
+				. \esc_html__( 'Settings', 'wp-search-with-algolia' )
+				. '</a>',
 			]
 		);
 	}
@@ -116,90 +138,15 @@ class Options extends Service {
 	 * @since  2.0.0
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 */
-	public function admin_menu() {
+	public function admin_menu(): void {
 		\add_menu_page(
 			\esc_html__( 'WP Search with Algolia', 'wp-search-with-algolia' ),
 			\esc_html__( 'Algolia Settings', 'wp-search-with-algolia' ),
 			'manage_options',
 			$this->slug,
-			[ $this, 'display_options_page' ],
+			[ $this, 'render_page' ],
 			'dashicons-welcome-widgets-menus',
 			90
-		);
-	}
-
-	/**
-	 * Register and add settings.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function admin_init() {
-		$this->register_settings();
-		$this->add_settings();
-	}
-
-	/**
-	 * Register settings.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function register_settings() {
-		\register_setting(
-			$this->option_group,
-			'algolia_application_id',
-			[
-				'type'              => 'string',
-				'description'       => \esc_html__( 'Algolia Application ID', 'wp-search-with-algolia' ),
-				'sanitize_callback' => 'sanitize_text_field',
-				'show_in_rest'      => false,
-				'default'           => '',
-			]
-		);
-		\register_setting(
-			$this->option_group,
-			'algolia_api_key',
-			[
-				'type'              => 'string',
-				'description'       => \esc_html__( 'Algolia Admin API Key', 'wp-search-with-algolia' ),
-				'sanitize_callback' => 'sanitize_text_field',
-				'show_in_rest'      => false,
-				'default'           => '',
-			]
-		);
-		\register_setting(
-			$this->option_group,
-			'algolia_search_api_key',
-			[
-				'type'              => 'string',
-				'description'       => \esc_html__( 'Algolia Search API Key', 'wp-search-with-algolia' ),
-				'sanitize_callback' => 'sanitize_text_field',
-				'show_in_rest'      => false,
-				'default'           => '',
-			]
-		);
-		\register_setting(
-			$this->option_group,
-			'algolia_index_name_prefix',
-			[
-				'type'              => 'string',
-				'description'       => \esc_html__( 'Prefix to use for Indexes', 'wp-search-with-algolia' ),
-				'sanitize_callback' => 'sanitize_text_field',
-				'show_in_rest'      => false,
-				'default'           => '',
-			]
-		);
-		\register_setting(
-			$this->option_group,
-			'algolia_powered_by_enabled',
-			[
-				'type'              => 'string',
-				'description'       => \esc_html__( 'Displays or Removes Powered By Algolia logo', 'wp-search-with-algolia' ),
-				'sanitize_callback' => 'sanitize_text_field',
-				'show_in_rest'      => false,
-				'default'           => '',
-			]
 		);
 	}
 
@@ -209,52 +156,12 @@ class Options extends Service {
 	 * @since  2.0.0
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 */
-	public function add_settings() {
+	public function add_settings_section(): void {
 		\add_settings_section(
 			$this->section,
 			null,
-			[ $this, 'display_settings_section' ],
+			[ $this, 'render_settings_section' ],
 			$this->slug
-		);
-
-		\add_settings_field(
-			'algolia_application_id',
-			\esc_html__( 'Application ID', 'wp-search-with-algolia' ),
-			[ $this, 'application_id_field' ],
-			$this->slug,
-			$this->section
-		);
-
-		\add_settings_field(
-			'algolia_search_api_key',
-			\esc_html__( 'Search-only API key', 'wp-search-with-algolia' ),
-			[ $this, 'search_key_field' ],
-			$this->slug,
-			$this->section
-		);
-
-		\add_settings_field(
-			'algolia_api_key',
-			\esc_html__( 'Admin API key', 'wp-search-with-algolia' ),
-			[ $this, 'api_key_field' ],
-			$this->slug,
-			$this->section
-		);
-
-		\add_settings_field(
-			'algolia_index_name_prefix',
-			\esc_html__( 'Index name prefix', 'wp-search-with-algolia' ),
-			[ $this, 'index_prefix_field' ],
-			$this->slug,
-			$this->section
-		);
-
-		\add_settings_field(
-			'algolia_powered_by_enabled',
-			\esc_html__( 'Remove Algolia powered by logo', 'wp-search-with-algolia' ),
-			[ $this, 'powered_by_field' ],
-			$this->slug,
-			$this->section
 		);
 	}
 
@@ -264,7 +171,7 @@ class Options extends Service {
 	 * @since  2.0.0
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 */
-	public function display_options_page() {
+	public function render_page(): void {
 		include_once WPSWA_PLUGIN_DIR . '/src/Views/Admin/Options.php';
 	}
 
@@ -274,8 +181,9 @@ class Options extends Service {
 	 * @since  2.0.0
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 */
-	public function display_settings_section() {
+	public function render_settings_section(): void {
 		?>
+
 		<p>
 			<?php \esc_html_e( 'Configure your Algolia account credentials. You can find them in the "API Keys" section of your Algolia dashboard.', 'wp-search-with-algolia' ); ?>
 		</p>
@@ -294,90 +202,6 @@ class Options extends Service {
 			?>
 		</p>
 
-		<?php
-
-	}
-
-	/**
-	 * Callback to display the App ID field.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function application_id_field() {
-		$setting       = $this->algolia_settings->get_app_id();
-		$disabled_html = $this->algolia_settings->is_app_id_in_config() ? ' disabled' : '';
-		?>
-		<input type="text" name="algolia_application_id" class="regular-text" value="<?php echo \esc_attr( $setting ); ?>" <?php echo \esc_html( $disabled_html ); ?>/>
-		<p class="description" id="home-description">
-			<?php \esc_html_e( 'Your Algolia Application ID.', 'wp-search-with-algolia' ); ?>
-		</p>
-		<?php
-	}
-
-	/**
-	 * Callback to display the Search Key field.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function search_key_field() {
-		$setting       = $this->algolia_settings->get_search_key();
-		$disabled_html = $this->algolia_settings->is_search_key_in_config() ? ' disabled' : '';
-		?>
-		<input type="text" name="algolia_search_api_key" class="regular-text" value="<?php echo \esc_attr( $setting ); ?>" <?php echo \esc_html( $disabled_html ); ?>/>
-		<p class="description" id="home-description">
-			<?php \esc_html_e( 'Your Algolia Search-only API key (public).', 'wp-search-with-algolia' ); ?>
-		</p>
-		<?php
-	}
-
-	/**
-	 * Callback to display the API Key field.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function api_key_field() {
-		$setting       = $this->algolia_settings->get_api_key();
-		$disabled_html = $this->algolia_settings->is_api_key_in_config() ? ' disabled' : '';
-		?>
-		<input type="password" name="algolia_api_key" class="regular-text" value="<?php echo \esc_attr( $setting ); ?>" <?php echo \esc_html( $disabled_html ); ?>/>
-		<p class="description" id="home-description">
-			<?php \esc_html_e( 'Your Algolia ADMIN API key (kept private).', 'wp-search-with-algolia' ); ?>
-		</p>
-		<?php
-	}
-
-	/**
-	 * Callback to display the Index Prefix field.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function index_prefix_field() {
-		$setting       = $this->algolia_settings->get_index_prefix();
-		$disabled_html = $this->algolia_settings->is_index_refix_in_config() ? ' disabled' : '';
-		?>
-		<input type="text" name="algolia_index_name_prefix" value="<?php echo \esc_attr( $setting ); ?>" <?php echo \esc_html( $disabled_html ); ?>/>
-		<p class="description" id="home-description">
-			<?php \esc_html_e( 'This prefix will be prepended to your index names.', 'wp-search-with-algolia' ); ?>
-		</p>
-		<?php
-	}
-
-	/**
-	 * Callback to display the Powered By field.
-	 *
-	 * @since  2.0.0
-	 * @author WebDevStudios <contact@webdevstudios.com>
-	 */
-	public function powered_by_field() {
-		?>
-		<input type="checkbox" name="algolia_powered_by_enabled" value="no" <?php checked( 'no', $this->algolia_settings->powered_by_enabled() ); ?> />
-		<p class="description" id="home-description">
-			<?php \esc_html_e( 'This will remove the Algolia logo from the autocomplete and the search page. We require that you keep the Algolia logo if you are using a free plan.', 'wp-search-with-algolia' ); ?>
-		</p>
 		<?php
 	}
 }
