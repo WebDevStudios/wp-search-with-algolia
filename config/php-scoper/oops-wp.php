61a38b1e941dda86e231c4d913ff18458b465313
Richard Aber <richard.aber@webdevstudios.com>
2020-02-28 11:20:15 -0600

    #20 Add PHP-Scoper config for OOPS

---
+++ config/php-scoper/oops-wp.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PHP-Scoper : OOPS-WP configuration file.
+ *
+ * @link https://github.com/humbug/php-scoper/blob/master/src/scoper.inc.php.tpl
+ *
+ * @author WebDevStudios <contact@webdevstudios.com>
+ * @since  2.0.0
+ *
+ * @package WebDevStudios\WPSWA
+ */
+
+declare( strict_types=1 );
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+return [
+
+	/*
+	 * By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
+	 * directory. You can however define which files should be scoped by defining a collection of Finders in the
+	 * following configuration key.
+	 *
+	 * For more see: https://github.com/humbug/php-scoper#finders-and-paths
+	 */
+	'finders'  => [
+		Finder::create()->files()->in( 'vendor/webdevstudios/oops-wp' ),
+	],
+
+	/*
+	 * When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
+	 * original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
+	 * support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
+	 * heart contents.
+	 *
+	 * For more see: https://github.com/humbug/php-scoper#patchers
+	 */
+	'patchers' => [],
+];
