9b03d63adb3ff49ba19169eae7b8c0145208df7c
Richard Aber <richard.aber@webdevstudios.com>
2020-03-23 21:35:20 -0500

    #20 Functioning Bundled Script without hardcoded keys

--- src/Services/Assets/Scripts/AlgoliaBundleScript.php
+++ src/Services/Assets/Scripts/AlgoliaBundleScript.php
@@ -8,9 +8,11 @@
 
 namespace WebDevStudios\WPSWA\Services\Assets\Scripts;
 
-use \WebDevStudios\WPSWA\Services\Assets\Script;
-use \WebDevStudios\WPSWA\Contracts\RegisterableAsset;
-use \WebDevStudios\WPSWA\Contracts\EnqueueableAsset;
+use WebDevStudios\WPSWA\Services\Assets\Script;
+use WebDevStudios\WPSWA\Contracts\RegisterableAsset;
+use WebDevStudios\WPSWA\Contracts\EnqueueableAsset;
+use WebDevStudios\WPSWA\Utility\AlgoliaSettings;
+use WebDevStudios\WPSWA\Utility\Requirements;
 
 /**
  * Class AlgoliaBundleScript
@@ -64,7 +66,7 @@ class AlgoliaBundleScript extends Script implements RegisterableAsset, Enqueueab
 	 *
 	 * @var string|bool|null
 	 */
-	public $ver = '2.0.0';
+	public $ver = WPSWA_PLUGIN_VERSION;
 
 	/**
 	 * Optional.
@@ -78,12 +80,26 @@ class AlgoliaBundleScript extends Script implements RegisterableAsset, Enqueueab
 	public $in_footer = true;
 
 	/**
-	 * Register hooks.
+	 * The Algolia Settings.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @Inject
+	 * @var AlgoliaSettings
+	 */
+	protected $settings = null;
+
+	/**
+	 * AlgoliaBundleScript constructor.
 	 *
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 * @since  2.0.0
+	 *
+	 * @param AlgoliaSettings $algolia_settings     The Algolia Settings.
 	 */
-	public function register_hooks() {
+	public function __construct( AlgoliaSettings $algolia_settings ) {
+		$this->settings = $algolia_settings;
+
 		/**
 		 * Filters the `$in_footer` argument to `wp_register_script()` for Algolia Scripts.
 		 *
@@ -92,9 +108,17 @@ class AlgoliaBundleScript extends Script implements RegisterableAsset, Enqueueab
 		 * @param bool $in_footer Whether to enqueue the script before </body> instead of in the <head>. Default 'false'.
 		 */
 		$this->in_footer = (bool) apply_filters( 'algolia_load_scripts_in_footer', $this->in_footer );
+	}
 
-		\add_action( 'wp_enqueue_scripts', [ $this, 'register_asset' ] );
-		\add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_asset' ] );
+	/**
+	 * Register hooks.
+	 *
+	 * @author WebDevStudios <contact@webdevstudios.com>
+	 * @since  2.0.0
+	 */
+	public function register_hooks() {
+		add_action( 'wp_enqueue_scripts', [ $this, 'register_asset' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_asset' ] );
 	}
 
 	/**
@@ -122,6 +146,63 @@ class AlgoliaBundleScript extends Script implements RegisterableAsset, Enqueueab
 	public function enqueue_asset() {
 		wp_enqueue_script(
 			$this->handle
+		);
+		$this->add_inline_script();
+	}
+
+	/**
+	 * Add inline script.
+	 *
+	 * @todo This needs a lot of work.
+	 *       The searchable indices need to be available to the JS.
+	 *       Currently the bundled JS is not configurable nor customizable.
+	 *
+	 * @author WebDevStudios <contact@webdevstudios.com>
+	 * @since  2.0.0
+	 */
+	public function add_inline_script() {
+
+		/**
+		 * Filters the array of configuration parameters exposed for Algolia JS.
+		 *
+		 * @author WebDevStudios <contact@webdevstudios.com>
+		 * @since  2.0.0
+		 *
+		 * @param array $config {
+		 *     Algolia JS configuration.
+		 *
+		 *     @type bool   $debug         Debug mode enabled or not.
+		 *     @type string $applicationID The Algolia Application ID.
+		 *     @type string $apiKey        The Search-only API key.
+		 *     @type array  $indices       Array of available Algolia Indices.
+		 *     @type array  $searchBox     Array of searchBox widget options.
+		 *     @type array  $hits          Array of hits widget options.
+		 * }
+		 */
+		$config = (array) apply_filters(
+			'wpswa_js_config',
+			[
+				'debug'         => defined( 'WP_DEBUG' ) ? WP_DEBUG : false,
+				'applicationID' => $this->settings->get_app_id(),
+				'apiKey'        => $this->settings->get_search_key(),
+				'indices'       => [
+					'searchablePosts' => $this->settings->get_index_prefix() . 'searchable_posts',
+				],
+				'searchBox'     => [
+					'container' => '.searchform',
+					'limit'     => 5,
+					'showMore'  => true,
+				],
+				'hits'          => [
+					'container' => '#content',
+				],
+			]
+		);
+
+		wp_add_inline_script(
+			$this->handle,
+			'wpswaBundleConfig = ' . wp_json_encode( $config ),
+			'before'
 		);
 	}
 }
