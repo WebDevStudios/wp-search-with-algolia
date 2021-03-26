4d946d19b6586d5332f5b8c78cb73e118fc91e1d
Ashar Irfan <mrasharirfan@gmail.com>
2021-03-27 00:04:03 +0500

    Include the update messages class in classmap

--- classmap.php
+++ classmap.php
@@ -45,6 +45,7 @@ require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-term-changes-watche
 require_once ALGOLIA_PATH . 'includes/watchers/class-algolia-user-changes-watcher.php';
 
 require_once ALGOLIA_PATH . 'includes/utilities/class-algolia-template-utils.php';
+require_once ALGOLIA_PATH . 'includes/utilities/class-algolia-update-messages.php';
 
 if ( is_admin() ) {
 	require_once ALGOLIA_PATH . 'includes/admin/class-algolia-admin.php';
