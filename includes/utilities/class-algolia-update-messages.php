c114f2a85a713e4a6883750f16ff94f96da043a9
Richard Aber <richard.aber@webdevstudios.com>
2021-04-30 14:22:43 -0500

    Update dockblock

--- includes/utilities/class-algolia-update-messages.php
+++ includes/utilities/class-algolia-update-messages.php
@@ -31,12 +31,38 @@ class Algolia_Update_Messages {
 	}
 
 	/**
-	 * Major release notice for versions less than 2.0.0
+	 * Update notice.
 	 *
 	 * @author WebDevStudios <contact@webdevstudios.com>
 	 * @since  1.8.0-dev
 	 *
-	 * @param array $plugin_data Plugin data.
+	 * @param array  $plugin_data {
+	 *     An array of plugin metadata.
+	 *
+	 *     @type string $name        The human-readable name of the plugin.
+	 *     @type string $plugin_uri  Plugin URI.
+	 *     @type string $version     Plugin version.
+	 *     @type string $description Plugin description.
+	 *     @type string $author      Plugin author.
+	 *     @type string $author_uri  Plugin author URI.
+	 *     @type string $text_domain Plugin text domain.
+	 *     @type string $domain_path Relative path to the plugin's .mo file(s).
+	 *     @type bool   $network     Whether the plugin can only be activated network wide.
+	 *     @type string $title       The human-readable title of the plugin.
+	 *     @type string $author_name Plugin author's name.
+	 *     @type bool   $update      Whether there's an available update. Default null.
+	 * }
+	 *
+	 * @param object $response {
+	 *     An array of metadata about the available plugin update.
+	 *
+	 *     @type int    $id          Plugin ID.
+	 *     @type string $slug        Plugin slug.
+	 *     @type string $new_version New plugin version.
+	 *     @type string $url         Plugin URL.
+	 *     @type string $package     Plugin update package URL.
+	 * }
+	 *
 	 * @return void
 	 */
 	public function major_release_notice( $plugin_data ) {
