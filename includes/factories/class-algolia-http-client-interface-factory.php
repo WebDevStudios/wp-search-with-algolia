e0a034531982a9bcc13c7f8e656f5830153bc269
Richard Aber <richard.aber@webdevstudios.com>
2021-05-28 14:48:43 -0500

    Update Algolia_Http_Client_Interface_Factory to work with Algolia 3.x

--- includes/factories/class-algolia-http-client-interface-factory.php
+++ includes/factories/class-algolia-http-client-interface-factory.php
@@ -8,7 +8,7 @@
 
 use Algolia\AlgoliaSearch\Http\Guzzle6HttpClient;
 use Algolia\AlgoliaSearch\Http\HttpClientInterface;
-use Algolia\AlgoliaSearch\Http\Php53HttpClient;
+use Algolia\AlgoliaSearch\Http\CurlHttpClient;
 
 /**
  * Class Algolia_Http_Client_Interface_Factory
@@ -41,38 +41,79 @@ class Algolia_Http_Client_Interface_Factory {
 			return $http_client;
 		}
 
-		$http_client = self::create_php53_http_client();
+		$http_client = self::create_http_client();
 
 		return $http_client;
 	}
 
 	/**
-	 * Create a Php53HttpClient client.
+	 * Create HttpClientInterface.
+	 *
+	 * @author     WebDevStudios <contact@webdevstudios.com>
+	 * @since      1.6.0
+	 *
+	 * @deprecated 2.0.0 Use Algolia_Http_Client_Interface_Factory::create_http_client()
+	 * @see        Algolia_Http_Client_Interface_Factory::create_http_client()
+	 *
+	 * @return HttpClientInterface
+	 */
+	public static function create_php53_http_client(): HttpClientInterface {
+		_deprecated_function(
+			__METHOD__,
+			'2.0.0',
+			'Algolia_Http_Client_Interface_Factory::create_http_client();'
+		);
+		return self::create_http_client();
+	}
+
+	/**
+	 * Create HttpClientInterface.
+	 *
+	 * Note: Algolia PHP Client 2.x used the `Php53HttpClient` class
+	 * when Guzzle was not available.
+	 * Algolia PHP Client v 3.x changed that class to `CurlHttpClient`.
+	 * Ideally we would implement Guzzle support at some point,
+	 * but we need to explore PHP Scoper or Mozart before doing so.
 	 *
 	 * @author WebDevStudios <contact@webdevstudios.com>
-	 * @since  1.6.0
+	 * @since  2.0.0
 	 *
-	 * @return Php53HttpClient
+	 * @return HttpClientInterface
 	 */
-	public static function create_php53_http_client(): Php53HttpClient {
+	public static function create_http_client(): HttpClientInterface {
+
+		$options = [];
 
 		/**
-		 * Allow developers to override the Php53HttpClient cURL options.
+		 * Allow developers to override the HttpClientInterface options.
+		 *
+		 * @author     WebDevStudios <contact@webdevstudios.com>
+		 * @since      1.6.0
+		 * @deprecated 2.0.0 Use {@see 'algolia_http_client_options'} instead.
+		 *
+		 * @param array $options Options for HttpClientInterface construction.
+		 */
+		$options = (array) apply_filters_deprecated(
+			'algolia_php_53_http_client_options',
+			[ $options ],
+			'2.0.0',
+			'algolia_http_client_options'
+		);
+
+		/**
+		 * Allow developers to override the HttpClientInterface options.
 		 *
 		 * @link   https://www.algolia.com/doc/api-client/advanced/pass-options-to-the-http-client/php/?language=php#using-the-default-php53httpclient
 		 * @link   https://www.algolia.com/doc/api-client/getting-started/upgrade-guides/php/#curl-options
 		 * @link   https://curl.haxx.se/libcurl/c/curl_easy_setopt.html
 		 *
 		 * @author WebDevStudios <contact@webdevstudios.com>
-		 * @since  1.6.0
+		 * @since  2.0.0
 		 *
-		 * @param array $options Curl options for Php53HttpClient construction.
+		 * @param array $options Options for HttpClientInterface construction.
 		 */
-		$options = apply_filters(
-			'algolia_php_53_http_client_options',
-			[]
-		);
+		$options = (array) apply_filters( 'algolia_http_client_options', $options );
 
-		return new Php53HttpClient( $options );
+		return new CurlHttpClient( $options );
 	}
 }
