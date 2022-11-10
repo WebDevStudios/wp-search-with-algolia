b19cc9feaa9e043de26662da26452d55e96e86e5
Michael Beckwith <tw2113@gmail.com>
2022-11-10 15:09:47 -0600

    update autocomplete file path

--- includes/class-algolia-scripts.php
+++ includes/class-algolia-scripts.php
@@ -54,10 +54,9 @@ class Algolia_Scripts {
 
 		wp_register_script(
 			'algolia-autocomplete',
-			ALGOLIA_PLUGIN_URL . 'js/autocomplete.js/dist/autocomplete' . $suffix . '.js',
+			ALGOLIA_PLUGIN_URL . 'js/autocomplete.js/dist/umd/index' . $ais_suffix . '.js',
 			[
 				'underscore',
-				'wp-util',
 				'algolia-search',
 			],
 			ALGOLIA_VERSION,
