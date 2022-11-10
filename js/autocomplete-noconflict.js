207cd2fe5f3de618c0228d038cb7272a8254d4d4
Michael Beckwith <tw2113@gmail.com>
2022-11-10 15:22:40 -0600

    i have to suspect all these parts are somehow required

--- js/autocomplete-noconflict.js
+++
@@ -1,1 +0,0 @@
-window.algoliaAutocomplete = autocomplete.noConflict();
--- js/instantsearch.js/es/lib/utils/escapeRefinement.js
+++
@@ -1,9 +0,0 @@
-function escapeRefinement(value) {
-  if (typeof value === 'number' && value < 0) {
-    value = String(value).replace(/^-/, '\\-');
-  }
-
-  return value;
-}
-
-export default escapeRefinement;
