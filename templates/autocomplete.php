fadf4d8425c9597a305edea265b9e65164f123ab
Michael Beckwith <tw2113@gmail.com>
2022-11-18 12:53:00 -0600

    current status of the autocomplete js

--- templates/autocomplete.php
+++ templates/autocomplete.php
@@ -72,122 +72,107 @@
 </script>
 
 <script type="text/javascript">
-	window.onload = function () {
+	window.addEventListener('load', function () {
+		const {autocomplete, getAlgoliaResults} = window['@algolia/autocomplete-js'];
+		const searchClient = algoliasearch(algolia.application_id, algolia.search_api_key);
 
-		/* Initialize Algolia client */
-		var client = algoliasearch( algolia.application_id, algolia.search_api_key );
-
-		/**
-		 * Algolia hits source method.
-		 *
-		 * This method defines a custom source to use with autocomplete.js.
-		 *
-		 * @param object $index Algolia index object.
-		 * @param object $params Options object to use in search.
-		 */
-		var algoliaHitsSource = function( index, params ) {
-			return function( query, callback ) {
-				index
-					.search( query, params )
-					.then( function( response ) {
-						callback( response.hits, response );
-					})
-					.catch( function( error ) {
-						callback( [] );
-					});
-			}
+		function get_sources(query) {
+			let sources = [];
+			algolia.autocomplete.sources.forEach(function (config, i) {
+				sources.push(
+					{
+						sourceId: config.index_id,
+						getItems() {
+							return getAlgoliaResults({
+								searchClient,
+								queries: [
+									{
+										indexName: config.index_name,
+										query,
+										params   : {
+											hitsPerPage: config['max_suggestions'],
+										},
+									},
+								],
+							});
+						},
+						templates: {
+							header: function ( { state, source, items, html } ) {
+								return html`
+									<div class="autocomplete-header">
+										<div class="autocomplete-header-title"></div>
+										<div class="clear"></div>
+									</div>`;
+							},
+							item( { item, components, html } ) {
+								for (let key in item._highlightResult) {
+									/* We do not deal with arrays. */
+									if (typeof item._highlightResult[key].value !== 'string') {
+										continue;
+									}
+									item._highlightResult[key].value = item._highlightResult[key].value.replace(/__aa-highlight__/g, '<em>').replace(/__\/aa-highlight__/g, '</em>');
+								}
+								return get_template( config['tmpl_suggestion'], item, html, components);
+							}
+						}
+					}
+				)
+			});
+			return sources;
 		}
 
-		/* Setup autocomplete.js sources */
-		var sources = [];
-		algolia.autocomplete.sources.forEach( function( config, i ) {
-			var suggestion_template = wp.template( config[ 'tmpl_suggestion' ] );
-			sources.push( {
-				source: algoliaHitsSource( client.initIndex( config[ 'index_name' ] ), {
-					hitsPerPage: config[ 'max_suggestions' ],
-					attributesToSnippet: [
-						'content:10'
-					],
-					highlightPreTag: '__ais-highlight__',
-					highlightPostTag: '__/ais-highlight__'
-				} ),
-				templates: {
-					header: function () {
-						return wp.template( 'autocomplete-header' )( {
-							label: _.escape( config[ 'label' ] )
-						} );
+		function get_template(template, item, html, components) {
+			let value = '';
+			switch (template) {
+				case 'autocomplete-post-suggestion':
+					value = html`
+						<div><a class="suggestion-link" href="${item.permalink}" title="${item.post_title}">
+							<div class="suggestion-post-attributes">
+								<span class="suggestion-post-title">${item._highlightResult.post_title.value}</span>
+							</div>
+						</a></div>`;
+					break;
+			}
+			return value;
+		}
+
+		let config = {
+			container  : '#autocomplete',
+			openOnFocus: true,
+			getSources( { query } ) {
+				if ( ! query ) {
+					return [];
+				}
+				return get_sources(query);
+			},
+			templates(){
+				return {
+					header() {
+						console.log('h');
+						let g = 'g';
+						return '';
 					},
-					suggestion: function ( hit ) {
-						if ( hit.escaped === true ) {
-							return suggestion_template( hit );
-						}
-						hit.escaped = true;
+					item() {
+						console.log('i');
+						let g = 'g';
+						return '';
+					},
+					footer({state, source, items, html}) {
+						console.log('f');
+						let g = '';
+						return html`
+									<div class="autocomplete-footer">
+										<div class="autocomplete-footer-branding">
+											<a href="#" class="algolia-powered-by-link" title="Algolia">
 
-						for ( var key in hit._highlightResult ) {
-							/* We do not deal with arrays. */
-							if ( typeof hit._highlightResult[ key ].value !== 'string' ) {
-								continue;
-							}
-							hit._highlightResult[ key ].value = _.escape( hit._highlightResult[ key ].value );
-							hit._highlightResult[ key ].value = hit._highlightResult[ key ].value.replace( /__ais-highlight__/g, '<em>' ).replace( /__\/ais-highlight__/g, '</em>' );
-						}
-
-						for ( var key in hit._snippetResult ) {
-							/* We do not deal with arrays. */
-							if ( typeof hit._snippetResult[ key ].value !== 'string' ) {
-								continue;
-							}
-
-							hit._snippetResult[ key ].value = _.escape( hit._snippetResult[ key ].value );
-							hit._snippetResult[ key ].value = hit._snippetResult[ key ].value.replace( /__ais-highlight__/g, '<em>' ).replace( /__\/ais-highlight__/g, '</em>' );
-						}
-
-						return suggestion_template( hit );
+											</a>
+										</div>
+									</div>`;
 					}
 				}
-			} );
-
-		} );
-
-		/* Setup dropdown menus */
-		document.querySelectorAll( algolia.autocomplete.input_selector ).forEach( function( element ) {
-
-			var config = {
-				debug: algolia.debug,
-				hint: false,
-				openOnFocus: true,
-				appendTo: 'body',
-				templates: {
-					empty: wp.template( 'autocomplete-empty' )
-				}
-			};
-
-			if ( algolia.powered_by_enabled ) {
-				config.templates.footer = wp.template( 'autocomplete-footer' );
 			}
-
-			/* Instantiate autocomplete.js */
-			var autocomplete = algoliaAutocomplete( element, config, sources )
-				.on( 'autocomplete:selected', function ( e, suggestion ) {
-					/* Redirect the user when we detect a suggestion selection. */
-					window.location.href = suggestion.permalink;
-				} );
-
-			/* Force the dropdown to be re-drawn on scroll to handle fixed containers. */
-			window.addEventListener( 'scroll', function() {
-				if ( autocomplete.autocomplete.getWrapper().style.display === "block" ) {
-					autocomplete.autocomplete.close();
-					autocomplete.autocomplete.open();
-				}
-			} );
-		} );
-
-		var algoliaPoweredLink = document.querySelector( '.algolia-powered-by-link' );
-		if ( algoliaPoweredLink ) {
-			algoliaPoweredLink.addEventListener( 'click', function( e ) {
-				e.preventDefault();
-				window.location = "https://www.algolia.com/?utm_source=WordPress&utm_medium=extension&utm_content=" + window.location.hostname + "&utm_campaign=poweredby";
-			} );
 		}
-	};
+
+		autocomplete(config);
+	});
 </script>
