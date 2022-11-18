6d7ebb5566f804f444e903b5db24aa2133094962
Michael Beckwith <tw2113@gmail.com>
2022-11-18 17:30:07 -0600

    move our user and term templates into our new function

--- templates/autocomplete.php
+++ templates/autocomplete.php
@@ -25,28 +25,6 @@
 	</a>
 </script>
 
-<script type="text/html" id="tmpl-autocomplete-term-suggestion">
-	<a class="suggestion-link" href="{{ data.permalink }}" title="{{ data.name }}">
-		<svg viewBox="0 0 21 21" width="21" height="21">
-			<svg width="21" height="21" viewBox="0 0 21 21">
-				<path
-					d="M4.662 8.72l-1.23 1.23c-.682.682-.68 1.792.004 2.477l5.135 5.135c.7.693 1.8.688 2.48.005l1.23-1.23 5.35-5.346c.31-.31.54-.92.51-1.36l-.32-4.29c-.09-1.09-1.05-2.06-2.15-2.14l-4.3-.33c-.43-.03-1.05.2-1.36.51l-.79.8-2.27 2.28-2.28 2.27zm9.826-.98c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25z"
-					fill-rule="evenodd"></path>
-			</svg>
-		</svg>
-		<span class="suggestion-post-title">{{{ data._highlightResult.name.value }}}</span>
-	</a>
-</script>
-
-<script type="text/html" id="tmpl-autocomplete-user-suggestion">
-	<a class="suggestion-link user-suggestion-link" href="{{ data.posts_url }}" title="{{ data.display_name }}">
-		<# if ( data.avatar_url ) { #>
-			<img class="suggestion-user-thumbnail" src="{{ data.avatar_url }}" alt="{{ data.display_name }}">
-		<# } #>
-		<span class="suggestion-post-title">{{{ data._highlightResult.display_name.value }}}</span>
-	</a>
-</script>
-
 <script type="text/html" id="tmpl-autocomplete-footer">
 	<div class="autocomplete-footer">
 		<div class="autocomplete-footer-branding">
@@ -130,6 +108,24 @@
 								</span>
 							</div>
 						</a></div>`;
+					break;
+				case 'autocomplete-user-suggestion':
+					value = html`<div><a class="suggestion-link user-suggestion-link" href="${item.posts_url}" title="${item.display_name}">
+							<img class="suggestion-user-thumbnail" src="${item.avatar_url}}" alt="${item.display_name}" />
+							<span class="suggestion-post-title">${item.display_name}</span>
+						</a></div>`;
+					break;
+				case 'autocomplete-term-suggestion':
+					value = html`<div><a class="suggestion-link" href="${ item.permalink }" title="${ item.name }">
+						<svg viewBox="0 0 21 21" width="21" height="21">
+							<svg width="21" height="21" viewBox="0 0 21 21">
+								<path
+									d="M4.662 8.72l-1.23 1.23c-.682.682-.68 1.792.004 2.477l5.135 5.135c.7.693 1.8.688 2.48.005l1.23-1.23 5.35-5.346c.31-.31.54-.92.51-1.36l-.32-4.29c-.09-1.09-1.05-2.06-2.15-2.14l-4.3-.33c-.43-.03-1.05.2-1.36.51l-.79.8-2.27 2.28-2.28 2.27zm9.826-.98c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25z"
+									fill-rule="evenodd"></path>
+							</svg>
+						</svg>
+						<span class="suggestion-post-title">${ item.name }</span>
+					</a></div>`
 					break;
 			}
 			return value;
