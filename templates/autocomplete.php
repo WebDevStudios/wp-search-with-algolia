b55ff8ce905b9514df8b8fde6f49b15f06ad699f
Michael Beckwith <tw2113@gmail.com>
2022-12-02 19:10:52 -0600

    re-add powered by link handling. not working yet

--- templates/autocomplete.php
+++ templates/autocomplete.php
@@ -129,6 +129,14 @@
 			return value;
 		}
 
+		let algoliaPoweredLink = document.querySelector('.algolia-powered-by-link');
+		if (algoliaPoweredLink) {
+			algoliaPoweredLink.addEventListener('click', function (e) {
+				e.preventDefault();
+				window.location = "https://www.algolia.com/?utm_source=WordPress&utm_medium=extension&utm_content=" + window.location.hostname + "&utm_campaign=poweredby";
+			});
+		}
+
 		let config = {
 			container  : '#autocomplete',
 			openOnFocus: true,
