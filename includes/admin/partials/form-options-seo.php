8f9fa14b9e68dc0893db2e358b146080f1a390b3
Michael Beckwith <tw2113@gmail.com>
2023-02-07 15:08:54 -0600

    replace settings content with static images representing our settings

--- includes/admin/partials/form-options-seo.php
+++ includes/admin/partials/form-options-seo.php
@@ -11,48 +11,8 @@
 ?>
 
 <div class="wrap">
-	<h1>WP Search with Algolia and Search Engine Optimization</h1>
-	<form method="post" action="options.php">
-		<h2>Noindex settings</h2>
-		<p>Respect a post or content type noindex status for various SEO plugins</p>
-		<table class="form-table" role="presentation">
-			<tbody>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_yoast_noindex">Yoast SEO</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_yoast_noindex" name="wpswa_premium_yoast_noindex" value="yes">
-					<label for="wpswa_premium_yoast_noindex"></label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_aioseo_noindex">All in One SEO</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_aioseo_noindex" name="wpswa_premium_aioseo_noindex" value="yes">
-					<label for="wpswa_premium_aioseo_noindex"></label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_rankmath_noindex">Rank Math SEO</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_rankmath_noindex" name="wpswa_premium_rankmath_noindex" value="yes" checked="checked">
-					<label for="wpswa_premium_rankmath_noindex"></label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_seoframework_noindex">The SEO Framework</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_seoframework_noindex" name="wpswa_premium_seoframework_noindex" value="yes">
-					<label for="wpswa_premium_seoframework_noindex"></label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_seopress_noindex">SEOPress</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_seopress_noindex" name="wpswa_premium_seopress_noindex" value="yes">
-					<label for="wpswa_premium_seopress_noindex"></label>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-	</form>
+	<h1>
+		<?php esc_html_e( 'WP Search with Algolia and Search Engine Optimization', 'wp-search-with-algolia' ); ?>
+	</h1>
+	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-premium-seo.png' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Premium.', 'wp-search-with-algolia' ); ?>" />
 </div>
--- includes/admin/partials/form-options-woocommerce.php
+++ includes/admin/partials/form-options-woocommerce.php
@@ -11,63 +11,8 @@
 ?>
 
 <div class="wrap">
-	<h1>WP Search with Algolia and WooCommerce</h1>
-	<form method="post" action="options.php">
-		<h2>Algolia search relevance</h2>
-		<p>These details aid in relevance and ranking.</p>
-		<table class="form-table" role="presentation">
-			<tbody>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_sku">Product SKU</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_sku" name="wpswa_premium_include_sku" value="yes" checked="checked">
-					<label for="wpswa_premium_include_sku">Include SKU value in the index</label>
-					<p>Useful to help look up products directly.</p>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_short_description">Short description</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_short_description" name="wpswa_premium_include_short_description" value="yes" checked="checked">
-					<label for="wpswa_premium_include_short_description">Include the short description in the index</label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_total_sales">Total sales</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_total_sales" name="wpswa_premium_include_total_sales" value="yes" checked="checked">
-					<label for="wpswa_premium_include_total_sales">Include total sales in index</label>
-					<p>Useful for popularity relevance. This will only be included for ranking and not template display.</p>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_ratings">Product rating</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_ratings" name="wpswa_premium_include_ratings" value="yes" checked="checked">
-					<label for="wpswa_premium_include_ratings">Include product cumulative rating in index</label>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-		<h2>Algolia results display</h2>
-		<p>These details aid in content indexed for template and results display.</p>
-		<table class="form-table" role="presentation">
-			<tbody>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_price">Price</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_price" name="wpswa_premium_include_price" value="yes" checked="checked">
-					<label for="wpswa_premium_include_price">Include price in index</label>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row"><label for="wpswa_premium_include_sale_price">Sale price</label></th>
-				<td>
-					<input type="checkbox" id="wpswa_premium_include_sale_price" name="wpswa_premium_include_sale_price" value="yes" checked="checked">
-					<label for="wpswa_premium_include_sale_price">Include sale price in index</label>
-				</td>
-			</tr>
-			</tbody>
-		</table>
-	</form>
+	<h1>
+		<?php esc_html_e( 'WP Search with Algolia and WooCommerce', 'wp-search-with-algolia' ); ?>
+	</h1>
+	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-premium-woocommerce.png' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Premium.', 'wp-search-with-algolia' ); ?>" />
 </div>
