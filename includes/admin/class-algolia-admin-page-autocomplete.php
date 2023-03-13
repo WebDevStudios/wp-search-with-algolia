7e73620acb5c49b42f7f310c88659b5dbd9e5b14
Michael Beckwith <tw2113@gmail.com>
2023-03-13 15:03:53 -0500

    update and clarify Autocomplete settings

--- includes/admin/class-algolia-admin-page-autocomplete.php
+++ includes/admin/class-algolia-admin-page-autocomplete.php
@@ -262,6 +262,8 @@ class Algolia_Admin_Page_Autocomplete {
 	 * @since  1.0.0
 	 */
 	public function print_section_settings() {
-		echo '<p>' . esc_html__( 'The autocomplete feature adds a find-as-you-type dropdown menu to your search bar(s).', 'wp-search-with-algolia' ) . '</p>';
+		echo '<p>' . esc_html__( 'Autocomplete adds a find-as-you-type dropdown to your search field(s).', 'wp-search-with-algolia' ) . '</p>';
+
+		echo '<p>' . esc_html__( 'Enabling Autocomplete adds the functionality to your site\'s frontend search. Indexing and settings pushes can be done regardless of enabled status.', 'wp-search-with-algolia' ) . '</p>';
 	}
 }
