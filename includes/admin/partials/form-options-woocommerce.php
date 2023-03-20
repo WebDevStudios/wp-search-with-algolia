298ab897cc685413fe8069e4bad93a4dcc9f9d87
Michael Beckwith <tw2113@gmail.com>
2023-03-20 14:10:54 -0500

    initial CTA with largely empty details

--- includes/admin/partials/form-options-woocommerce.php
+++ includes/admin/partials/form-options-woocommerce.php
@@ -14,4 +14,40 @@
 		<?php esc_html_e( 'WooCommerce', 'wp-search-with-algolia' ); ?>
 	</h1>
 	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-pro-woocommerce.png' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Pro.', 'wp-search-with-algolia' ); ?>" />
+	<div class="algolia-pro-cta">
+		<h2 class="algolia-pro-title">WP Search with Algolia Pro</h2>
+		<h3 class="algolia-pro-desc">
+			A premium Algolia search plugin specifically designed for enterprise-grade WordPress websites, including eCommerce
+		</h3>
+		<div class="algolia-pro-features">
+			<div>
+				<h4>WooCommerce Support</h4>
+				<span class="algolia-pro-feature">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0077ff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+					<span>Index your product SKUs and short descriptions</span>
+				</span>
+				<span class="algolia-pro-feature">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0077ff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+					<span>Index product total sales and total ratings</span>
+				</span>
+			</div>
+			<div>
+				<h4>Additional Features</h4>
+				<span class="algolia-pro-feature">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0077ff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+					<span>Fine tune indexing on selected pieces of content</span>
+				</span>
+				<span class="algolia-pro-feature">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#0077ff" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+					<span>Yoast SEO and All in One SEO Support</span>
+				</span>
+			</div>
+		</div>
+		<div>
+			<a href="https://pluginize.com/plugins/wp-search-with-algolia-pro/" class="algolia-pro-button" target="_blank" >Upgrade Now and Go Pro</a>
+			<span class="algolia-pro-more">
+				<a href="https://pluginize.com/plugins/wp-search-with-algolia-pro/" target="_blank">Learn more and see all the features</a>
+			</span>
+		</div>
+	</div>
 </div>
