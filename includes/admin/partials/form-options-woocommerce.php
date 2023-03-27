94719480d9114cab4c271a19924a52450d5ba0cd
Michael Beckwith <tw2113@gmail.com>
2023-03-27 10:27:42 -0500

    Feature/238 pro ctas (#315)
    * initial CTA with largely empty details
    
    * new utility static cta method
    
    * make use of utility method instead of hardcoding in both screens
    
    * tell to ignore escaping in these spots
    
    * phpdoc and misc spacing

--- includes/admin/partials/form-options-woocommerce.php
+++ includes/admin/partials/form-options-woocommerce.php
@@ -14,4 +14,5 @@
 		<?php esc_html_e( 'WooCommerce', 'wp-search-with-algolia' ); ?>
 	</h1>
 	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-pro-woocommerce.png' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Pro.', 'wp-search-with-algolia' ); ?>" />
+	<?php echo Algolia_Utils::pro_cta_content(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </div>
