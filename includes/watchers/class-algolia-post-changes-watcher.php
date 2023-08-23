0e225af18e4fc173965fcc5b2fd8ee9c3c55ecb3
Michael Beckwith <tw2113@gmail.com>
2023-08-23 13:03:15 -0500

    missed extra space

--- includes/watchers/class-algolia-post-changes-watcher.php
+++ includes/watchers/class-algolia-post-changes-watcher.php
@@ -82,7 +82,7 @@ class Algolia_Post_Changes_Watcher implements Algolia_Changes_Watcher {
 		add_action( 'add_attachment', array( $this, 'sync_item' ) );
 		add_action( 'attachment_updated', array( $this, 'sync_item' ) );
 		add_action( 'delete_attachment', array( $this, 'delete_item' ) );
-		add_action( 'pre_post_update', [ $this, 'check_slug_update' ],  10, 2 );
+		add_action( 'pre_post_update', [ $this, 'check_slug_update' ], 10, 2 );
 
 		// Support for WP All Import "fast mode".
 		add_action( 'pmxi_saved_post', [ $this, 'track_updated_posts' ] );
