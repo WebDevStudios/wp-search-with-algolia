08e1b5afddbe14749a9c17ebb480b6eca4a31b75
Michael Beckwith <tw2113@gmail.com>
2023-09-15 16:37:42 -0500

    inline docs for algolia config options

--- includes/class-algolia-template-loader.php
+++ includes/class-algolia-template-loader.php
@@ -95,6 +95,15 @@ class Algolia_Template_Loader {
 			'indices'              => [],
 			'autocomplete'         => [
 				'sources'        => $autocomplete_config->get_config(),
+
+				/**
+				 * Filters the CSS-style selector used to locate search inputs to add Autocomplete to.
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param  string $value Selector to target with.
+				 * @return string $value Updated selector.
+				 */
 				'input_selector' => (string) apply_filters( 'algolia_autocomplete_input_selector', "input[name='s']:not(.no-autocomplete):not(#adminbar-search)" ),
 			],
 		];
@@ -110,6 +119,15 @@ class Algolia_Template_Loader {
 		}
 
 		// Give developers a last chance to alter the configuration.
+		/**
+		 * Filters the final result of the algolia config object to be used.
+		 * Gives developers one last change to alter the configuration.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param  array $config Array of configuration options
+		 * @return array $config Final configuration.
+		 */
 		$config = (array) apply_filters( 'algolia_config', $config );
 
 		echo '<script type="text/javascript">var algolia = ' . wp_json_encode( $config ) . ';</script>';
