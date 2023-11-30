403af273a1bdc4ff7d15e1d9411c10cb5036f0eb
Michael Beckwith <tw2113@gmail.com>
2023-11-30 12:52:35 -0600

    update headings to match Pro, update file suffix

--- includes/admin/partials/form-options-woocommerce.php
+++ includes/admin/partials/form-options-woocommerce.php
@@ -11,8 +11,8 @@
 
 <div class="wrap">
 	<h1>
-		<?php esc_html_e( 'WooCommerce', 'wp-search-with-algolia' ); ?>
+		<?php esc_html_e( 'WP Search with Algolia and WooCommerce', 'wp-search-with-algolia' ); ?>
 	</h1>
-	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-pro-woocommerce.png' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Pro.', 'wp-search-with-algolia' ); ?>" />
+	<img style="max-width: 100%;" src="<?php echo esc_url( ALGOLIA_PLUGIN_URL . 'includes/admin/img/algolia-pro-woocommerce.jpg' ); ?>" alt="<?php esc_attr_e( 'Blurry representiation of features available with WP Search with Algolia Pro.', 'wp-search-with-algolia' ); ?>" />
 	<?php echo Algolia_Utils::pro_cta_content(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </div>
