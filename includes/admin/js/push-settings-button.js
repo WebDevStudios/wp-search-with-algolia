c1918ac3572fdd0626af9ed0b7d931b17041524d
Michael Beckwith <tw2113@gmail.com>
2024-10-17 19:05:37 -0500

    begin converting our push settings button conversion

--- includes/admin/js/push-settings-button.js
+++ includes/admin/js/push-settings-button.js
@@ -1,6 +1,84 @@
-(function($) {
+'use strict';
 
-	/* global algoliaPushSettingsButton */
+((window) => {
+	let buttons = document.querySelectorAll('.algolia-push-settings-button');
+	if (buttons) {
+		Array.from(buttons).forEach((button) => {
+			button.addEventListener('click', handleButtonClick);
+		});
+	}
+
+	function handleButtonClick(e) {
+		let clickedButton = e.currentTarget;
+		let algoliaIndex = clickedButton.dataset.index;
+		if (!algoliaIndex) {
+			throw new Error('Clicked button has no "data-index" set.');
+		}
+
+		if (!window.confirm(algoliaPushSettingsButton.pushBtnAlert)) {
+			return;
+		}
+
+		disableButton(clickedButton);
+
+		pushSettings(clickedButton, algoliaIndex);
+	}
+
+	function disableButton(button) {
+		button.disabled = true;
+	}
+
+	function enableButton(button) {
+		button.disabled = false;
+	}
+
+	function pushSettings(clickedButton, index) {
+
+		const data = new FormData();
+		data.append('action', 'algolia_push_settings');
+		data.append('index_id', index);
+
+		fetch(window.ajaxurl, options = {
+			method: 'POST',
+			body  : data,
+		})
+			.then((response) => response.json())
+			.then((response) => {
+				if (typeof response.success === 'undefined') {
+					alert('An error occurred');
+					enableButton(clickedButton);
+					return;
+				}
+
+				alert('Settings correctly pushed for index: ' + index);
+				enableButton(clickedButton);
+			})
+			.catch((error) => {
+				alert('An error occurred: ' + error.responseText);
+				enableButton(clickedButton);
+			});
+
+		$.post(
+			ajaxurl, data, function (response) {
+				if (typeof response.success === 'undefined') {
+					alert('An error occurred');
+					enableButton(clickedButton);
+					return;
+				}
+
+				alert('Settings correctly pushed for index: ' + index);
+				enableButton(clickedButton);
+			}
+		).fail(
+			function (response) {
+				alert('An error occurred: ' + response.responseText);
+				enableButton(clickedButton);
+			}
+		);
+	}
+})(window);
+
+(function($) {
 
 	$(
 		function() {
