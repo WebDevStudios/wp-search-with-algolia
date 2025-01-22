193493b0d1060de77e2b4c13e27af36136dac513
Michael Beckwith <tw2113@gmail.com>
2025-01-22 12:34:10 -0600

    SSL urls

--- templates/instantsearch.php
+++ templates/instantsearch.php
@@ -17,7 +17,7 @@ get_header();
 		<main id="ais-main">
 			<div class="algolia-search-box-wrapper">
 				<div id="algolia-search-box"></div>
-				<svg class="search-icon" width="25" height="25" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M24.828 31.657a16.76 16.76 0 0 1-7.992 2.015C7.538 33.672 0 26.134 0 16.836 0 7.538 7.538 0 16.836 0c9.298 0 16.836 7.538 16.836 16.836 0 3.22-.905 6.23-2.475 8.79.288.18.56.395.81.645l5.985 5.986A4.54 4.54 0 0 1 38 38.673a4.535 4.535 0 0 1-6.417-.007l-5.986-5.986a4.545 4.545 0 0 1-.77-1.023zm-7.992-4.046c5.95 0 10.775-4.823 10.775-10.774 0-5.95-4.823-10.775-10.774-10.775-5.95 0-10.775 4.825-10.775 10.776 0 5.95 4.825 10.775 10.776 10.775z" fill-rule="evenodd"></path></svg>
+				<svg class="search-icon" width="25" height="25" viewBox="0 0 40 40" xmlns="https://www.w3.org/2000/svg"><path d="M24.828 31.657a16.76 16.76 0 0 1-7.992 2.015C7.538 33.672 0 26.134 0 16.836 0 7.538 7.538 0 16.836 0c9.298 0 16.836 7.538 16.836 16.836 0 3.22-.905 6.23-2.475 8.79.288.18.56.395.81.645l5.985 5.986A4.54 4.54 0 0 1 38 38.673a4.535 4.535 0 0 1-6.417-.007l-5.986-5.986a4.545 4.545 0 0 1-.77-1.023zm-7.992-4.046c5.95 0 10.775-4.823 10.775-10.774 0-5.95-4.823-10.775-10.774-10.775-5.95 0-10.775 4.825-10.775 10.776 0 5.95 4.825 10.775 10.776 10.775z" fill-rule="evenodd"></path></svg>
 				<div id="algolia-stats"></div>
 				<div id="algolia-powered-by"></div>
 			</div>
@@ -142,7 +142,7 @@ get_header();
 								?>
 
 								return html`
-									<article itemtype="http://schema.org/Article">
+									<article itemtype="https://schema.org/Article">
 										${thumbnail}
 										<div class="ais-hits--content">
 											<h2 itemprop="name headline"><a href="${hit.permalink}" title="${hit.post_title}" class="ais-hits--title-link" itemprop="url">${components.Highlight({hit, attribute: 'post_title'})}</a></h2>
