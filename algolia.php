8e6500603dfb8ac58db9ebd8c90865cc28a119af
Michael Beckwith <tw2113@gmail.com>
2025-01-22 14:38:27 -0600

    update main php file version

--- algolia.php
+++ algolia.php
@@ -3,8 +3,13 @@
  * Plugin Name:       WP Search with Algolia
  * Plugin URI:        https://github.com/WebDevStudios/wp-search-with-algolia
  * Description:       Integrate the powerful Algolia search service with WordPress
+<<<<<<< HEAD
  * Version:           2.8.3
  * Requires at least: 5.3
+=======
+ * Version:           2.9.0
+ * Requires at least: 5.0
+>>>>>>> ddc3d54 (update main php file version)
  * Requires PHP:      7.4
  * Author:            WebDevStudios
  * Author URI:        https://webdevstudios.com
@@ -26,7 +31,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 // The Algolia Search plugin version.
-define( 'ALGOLIA_VERSION', '2.8.3' );
+define( 'ALGOLIA_VERSION', '2.9.0' );
 
 // The minmum required PHP version.
 define( 'ALGOLIA_MIN_PHP_VERSION', '7.4' );
