646876fbd5373c1f2d6474a015e983a55a5eefce
Michael Beckwith <tw2113@gmail.com>
2025-08-11 12:19:58 -0500

    add note regarding existing customized template files, and not changing setting

--- includes/admin/partials/form-override-search-version-option.php
+++ includes/admin/partials/form-override-search-version-option.php
@@ -48,4 +48,5 @@
 		);
 		?>
 	</div>
+	<p><strong><?php esc_html_e( 'Leave on current setting if you have InstantSearch customized via files in your active theme', 'wp-search-with-algolia' ); ?></strong></p>
 </div>
