74248a2ebf7ad998794919f5549978547c17b5ac
awaisAltafWebdevstudios <awais.altaf@webdevstudios.com>
2026-08-28 00:32:26 +0500

    Drop discouraged load_plugin_textdomain call in favor of core loading.

--- algolia.php
+++ algolia.php
@@ -132,8 +132,6 @@ function algolia_load_textdomain() {
 	$locale = apply_filters( 'plugin_locale', get_locale(), 'wp-search-with-algolia' );
 
 	load_textdomain( 'wp-search-with-algolia', WP_LANG_DIR . '/wp-search-with-algolia/wp-search-with-algolia-' . $locale . '.mo' );
-
-	load_plugin_textdomain( 'wp-search-with-algolia', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 
 add_action( 'init', 'algolia_load_textdomain' );
